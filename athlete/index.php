86563582042f4476b80f57ee1392263f7e1aa35d
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2024-03-06 13:44:33 +0530

    admin page developing

--- athlete/index.php
+++ athlete/index.php
@@ -11,6 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
+<!--- Navbar -->
 
   <div class="container mt-3">
     <h2>Athlete Dash Board</h2>

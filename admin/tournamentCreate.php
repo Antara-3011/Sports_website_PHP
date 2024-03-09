04aad230d8b8beffa1e73c8e0d60e08786e62f26
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2024-03-09 14:34:01 +0530

    complete create tournament

--- admin/tournamentCreate.php
+++ admin/tournamentCreate.php
@@ -19,42 +19,42 @@
     <div class="fromWrap">
   <form>
   <div class="form-row">
-   <div class="form-group col-md-6">
+   <div class="form-group col-md-12">
       <label for="inputTounamentName" class="inputHeading">Tournament Name</label>
       <input type="text" class="form-control" id="inputTounamentName" placeholder="Tournament name">
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-12">
       <label for="inputTounamentID" class="inputHeading">Tournament ID</label>
       <input type="text" class="form-control" id="inputTounamentID" placeholder="Tournament ID">
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-12">
       <label for="tournamentDate" class="inputHeading">Date of Event</label>
       <input type="date" class="form-control" id="tournamentDate" placeholder="Date">
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-12">
       <label for="tournamentVenue" class="inputHeading">Venue</label>
       <input type="text" class="form-control" id="tournamentVenue" placeholder="Venue">
     </div>
   </div>
-  <div class="form-group col-md-6">
+  <div class="form-group col-md-12">
       <label for="tournamentRegSt" class="inputHeading">Registration start date</label>
       <input type="date" class="form-control" id="tournamentRegSt" placeholder="Date">
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-12">
       <label for="tournamentRegEd" class="inputHeading">Registration end date</label>
       <input type="date" class="form-control" id="tournamentRegEd" placeholder="Date">
     </div>
     <h4 style="padding:6px;padding-top:20px;"><b>Single Point Of Contact</b></h4>
   <div class="form-row">
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-12">
       <label for="coachName" class="inputHeading"> Coach Name</label>
       <input type="text" class="form-control" id="coachName">
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-12">
       <label for="coachEmail" class="inputHeading">Email</label>
       <input type="email" class="form-control" id="coachEmail">
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-12">
       <label for="coachNumber" class="inputHeading">Phone Number</label>
       <input type="number" class="form-control" id="coachNumber">
     </div>
